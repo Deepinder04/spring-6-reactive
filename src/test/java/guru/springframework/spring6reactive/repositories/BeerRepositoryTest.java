7a0880b992a4198e5c07efef98784f606c418ccb
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-10-26 20:32:38 +0530

    added Bootstrap class for data

--- src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -6,12 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.context.annotation.Import;
-import reactor.core.publisher.Mono;
-
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
 @Import(DatabaseConfig.class)
