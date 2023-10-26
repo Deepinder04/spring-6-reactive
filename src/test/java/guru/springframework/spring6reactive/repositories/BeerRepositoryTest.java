f7deb2de3edb97638f277bc912b9278ab76b5861
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-10-26 09:10:55 +0530

    create and update timestamp with auditing

--- src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6reactive/repositories/BeerRepositoryTest.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring6reactive.repositories;
 
+import guru.springframework.spring6reactive.config.DatabaseConfig;
 import guru.springframework.spring6reactive.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
     @Autowired
